c5318552308864b0dca73159b661e52f8c9b8d44
jalvarro <jalvarro@LCG-2XWNVF2.usersad.everis.int>
2017-10-11 17:14:40 +0200

    OAuth2 server + client

--- src/main/java/com/aljumaro/test/oauth2/ssofacebook/SsoFacebookApplication.java
+++ src/main/java/com/aljumaro/test/oauth2/ssofacebook/SsoFacebookApplication.java
@@ -2,18 +2,23 @@ package com.aljumaro.test.oauth2.ssofacebook;
 
 import java.security.Principal;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.Filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -21,16 +26,22 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.CompositeFilter;
 
 @SpringBootApplication
 @EnableOAuth2Client
 @RestController
+@EnableAuthorizationServer
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SsoFacebookApplication extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -40,20 +51,28 @@ public class SsoFacebookApplication extends WebSecurityConfigurerAdapter {
 		SpringApplication.run(SsoFacebookApplication.class, args);
 	}
 
-	@GetMapping("/user")
-	public Principal user(Principal principal) {
-		return principal;
+	@RequestMapping({ "/user", "/me" })
+	public Map<String, String> user(Principal principal) {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		return map;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		
 		http.antMatcher("/**")
 			.authorizeRequests()
 				.antMatchers("/", "/login**", "/webjars/**").permitAll()
 				.anyRequest().authenticated()
 			.and().logout().logoutSuccessUrl("/").permitAll()
 			.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and().exceptionHandling()
+		      .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
 			.and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+		
+		// @formatter:on
 	}
 
 	private Filter ssoFilter(ClientResources client, String path) {
@@ -71,7 +90,8 @@ public class SsoFacebookApplication extends WebSecurityConfigurerAdapter {
 
 		CompositeFilter filter = new CompositeFilter();
 
-		filter.setFilters(Arrays.asList(ssoFilter(facebook(), "/login/facebook"), ssoFilter(github(), "/login/github")));
+		filter.setFilters(
+				Arrays.asList(ssoFilter(facebook(), "/login/facebook"), ssoFilter(github(), "/login/github")));
 
 		return filter;
 	}
@@ -79,13 +99,13 @@ public class SsoFacebookApplication extends WebSecurityConfigurerAdapter {
 	@Bean
 	@ConfigurationProperties("github")
 	public ClientResources github() {
-	  return new ClientResources();
+		return new ClientResources();
 	}
 
 	@Bean
 	@ConfigurationProperties("facebook")
 	public ClientResources facebook() {
-	  return new ClientResources();
+		return new ClientResources();
 	}
 
 	@Bean
@@ -94,6 +114,15 @@ public class SsoFacebookApplication extends WebSecurityConfigurerAdapter {
 		registration.setFilter(filter);
 		registration.setOrder(-100);
 		return registration;
+	}
+
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
+		}
 	}
 
 }
